3e312b6db9232fdedf3a2aefc29eb3b48208cc26
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2020-02-24 18:38:03 +0900

    seeder

--- database/seeds/CommentsTableSeeder.php
+++ database/seeds/CommentsTableSeeder.php
@@ -12,5 +12,25 @@ class CommentsTableSeeder extends Seeder
   public function run()
   {
     //
+    DB::table('comments')->insert([
+      'title' => '1つ目のコメント',
+      'body' => 'コメントの本文',
+      'created_at' => '2020-02-24 18:05:00',
+      'updated_at' => '2020-02-24 18:05:00'
+    ]);
+
+    DB::table('comments')->insert([
+        'title' => '2個目',
+        'body' => 'コメントのbody',
+        'created_at' => '2020-02-24 18:07:00',
+        'updated_at' => '2020-02-24 18:07:00'
+    ]);
+
+    DB::table('comments')->insert([
+        'title' => '三個目のタイトル',
+        'body' => '本文',
+        'created_at' => '2020-02-24 18:09:00',
+        'updated_at' => '2020-02-24 18:09:00'
+    ]);
   }
 }
