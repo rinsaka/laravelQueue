5e9e97e5c5d62e5c6cb6673b4350e7b1ee46531b
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2020-02-24 18:38:21 +0900

    routes & controller

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Comment;
 
 class CommentsController extends Controller
 {
-  //
+  public function index()
+  {
+    $comments = Comment::orderBy('created_at', 'DESC')
+                          ->get();
+    dd($comments);
+  }
 }
